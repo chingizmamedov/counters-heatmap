4eabc3ca57a02a10ff937006d382924c0ae41200
Chingiz Mamedov <38461842+chingiz92mi@users.noreply.github.com>
2020-04-22 19:25:50 +0400

    bug fixing

--- js/script.js
+++ js/script.js
@@ -75,7 +75,7 @@ const topTable = [
 const nameTips = ["kassa", "emeliyyat", "huquqi", "satish", "rehberlik"];
 const additionalBottomcards = ["totalFreeTime", "freeTC"];
 const mainLink = "/heatmap";
-const BASE_URL = "http://10.0.22.36:8080/QmaticMap/";
+const BASE_URL = "http://192.168.1.69:8080/QmaticMap/";
 const GET_POINTS = "getServicePointsData?branchId=";
 const GET_DATA = "getCountersData";
 const QMATIC_GET_PERCENT = "getMapPercentData";
